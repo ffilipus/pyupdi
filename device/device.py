98cac20df75a87a75b5d0a4aff303f8d9f7b1884
Filip Goldefus <fgoldefus@gmail.com>
2017-12-22 17:08:36 +0100

    Added ATTiny1614 device support.

--- device/device.py
+++ device/device.py
@@ -21,9 +21,18 @@ class Device(object): # pylint: disable=too-few-public-methods
             self.sigrow_address = 0x1100
             self.fuses_address = 0x1280
             self.userrow_address = 0x1300
+        elif device_name == "tiny1614":
+            self.flash_start = 0x8000
+            self.flash_size = 16 * 1024
+            self.flash_pagesize = 64
+            self.syscfg_address = 0x0F00
+            self.nvmctrl_address = 0x1000
+            self.sigrow_address = 0x1100
+            self.fuses_address = 0x1280
+            self.userrow_address = 0x1300
         else:
             raise Exception("Unknown device")
 
     @staticmethod
     def get_supported_devices():
-        return ["tiny817", "tiny816", "tiny814", "tiny417"]
+        return ["tiny817", "tiny816", "tiny814", "tiny417", "tiny1614"]
